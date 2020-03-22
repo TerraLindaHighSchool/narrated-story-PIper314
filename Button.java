edaac79996cd47412ccdf85f2c6bd000ed5d1246
Bruce Gustin <bgustin@srcs.org>
2020-03-22 11:46:06 -0700

    Added act to button

--- Button.java
+++ Button.java
@@ -32,6 +32,15 @@ public class Button extends Actor
     }
 
     /**
+     * Act - do whatever the Creature wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    
+    /**
      * Update the image on screen to show the current value.
      */
     private void updateImage()
